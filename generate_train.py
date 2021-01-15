c5e2ef00ac0f98de7425ac71f61226665e21d317
tuananh250696 <35196486+tuananh250696@users.noreply.github.com>
2021-01-16 01:13:52 +0700

    Update generate_train.py

--- generate_train.py
+++ generate_train.py
@@ -1,8 +1,4 @@
 from tkinter import *
-import sqlite3
-import tkinter.messagebox
-import sys
-# from PyQt5.QtWidgets import *
 
 #w = root.winfo_screenwidth()
 #h = root.winfo_screenheight()f
@@ -18,11 +14,8 @@ root.geometry("%dx%d+%d+%d" % (width, height, x, y))
 root.resizable(0, 0)
 
 #=======================================VARIABLES=====================================
-USERNAME = StringVar()
-PASSWORD = StringVar()
-FIRSTNAME = StringVar()
-LASTNAME = StringVar()
-
+# USERNAME = StringVar()
+# PASSWORD = StringVar()
 
 class Application:
     def __init__(self, master):
@@ -48,18 +41,34 @@ class Application:
         self.keyacticett = Label(self.left, text="Mã KEY:", font=('arial 12 bold'), fg='black',
                                  bg='lightblue')
         self.keyacticett.place(x=50, y=260)
-        self.adr_actice = Entry(self.left, font=('arial 20 bold'), width=40)
+        # self.adr_actice = Entry(self.left, font=('arial 20 bold'), width=40)
+        # self.adr_actice.place(x=60, y=310)
+        self.adr_actice = Text(root, height=1,width=40,bg="light yellow", font=('arial 20 bold'), fg='red')
         self.adr_actice.place(x=60, y=310)
-
         # button
         self.bt_st_catalog = Button(self.left, text="TẠO MÃ ACTICE", width=20, height=4, font=('arial 14 bold'),
-                                    bg='orange')
+                                    bg='orange',command=self.Take_input)
         self.bt_st_catalog.place(x=200, y=420)
 
         self.bt_exit1 = Button(self.left, text="Đóng", width=20, height=4, font=('arial 14 bold'), bg='orange')
         self.bt_exit1.place(x=480, y=420)
 
+    def Take_input(self):
+        # self.adr_actice.delete(0, END)
+        name_dtn1 = self.adr_id.get()
+        nnnn=0x203325467854764986
+        name_dtn222 = self.adr_mail.get()
+        n = len(name_dtn222) * 25061996 * len(name_dtn1)+len(name_dtn222) * 13022002 * len(name_dtn1)*nnnn
+        h1 = hex(n)
+        newstr = h1.replace("0x", "")
+        self.adr_actice.insert(END,newstr)
 
+        # INPUT = inputtxt.get("1.0", "end-1c")
+        # print(INPUT)
+        # if (INPUT == "120"):
+        #     Output.insert(END, 'Correct')
+        # else:
+        #     Output.insert(END, "Wrong answer")
 
 
 root.geometry("1024x600+0+0")
